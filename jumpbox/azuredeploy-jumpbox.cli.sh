f183fd6ca6e0dfa4502d24c150fc7fdd5de763e9
Rene Bouw <rene.bouw@rabobank.nl>
2016-10-31 08:51:00 +0100

    new attempt..

---
+++ jumpbox/azuredeploy-jumpbox.cli.sh
@@ -0,0 +1,2 @@
+azure group create -n cassandra -l "West Europe"
+azure group deployment create --template-file azuredeploy-jumpbox.json -e azuredeploy-jumpbox.parameters.json -g cassandra -n CassandraDeployment
