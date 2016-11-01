985fcc06acfa73c1a138cbdeb81d8a3c1184a659
Rene Bouw <rene.bouw@rabobank.nl>
2016-11-01 11:25:36 +0100

    fix node deployment; single node deployment possible again

--- jumpbox/azuredeploy-jumpbox.cli.sh
+++ jumpbox/azuredeploy-jumpbox.cli.sh
@@ -1,2 +1,2 @@
-azure group create -n cassandra -l "West Europe"
-azure group deployment create --template-file azuredeploy-jumpbox.json -e azuredeploy-jumpbox.parameters.json -g cassandra -n CassandraJBDeployment
+azure group create -n cassandraw -l "West Europe"
+azure group deployment create --template-file azuredeploy-jumpbox.json -e azuredeploy-jumpbox.parameters.json -g cassandraw -n CassandraJBDeployment
