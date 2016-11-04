33558d2249a0383226036d43807a34846cb43b18
Rene Bouw <rene.bouw@rabobank.nl>
2016-11-04 19:47:40 +0100

    fix

--- cassandra/azuredeploy-cassandra-node.sh
+++ cassandra/azuredeploy-cassandra-node.sh
@@ -7,8 +7,8 @@ if [[ $(id -u) -ne 0 ]] ; then
   exit 1
 fi
 
-if [ $# != 4 ]; then
-  echo "Usage: $0 <MasterHostname> <mountFolder> <numDataDisks> <adminUserName>"
+if [ $# != 3 ]; then
+  echo "Usage: $0 <MasterHostname> <numDataDisks> <adminUserName>"
   exit 1
 fi
 
