7954e07e4bed5920ebfcbe9177348bc536323d58
Rene Bouw <rene.bouw@rabobank.nl>
2016-11-20 17:00:10 +0100

    bigger disks, fix tunnel

--- cassandra/tunnel.sh
+++ cassandra/tunnel.sh
@@ -1,1 +1,1 @@
-ssh -N -L 9042:10.2.0.4:9042 -L 7199:10.2.0.4:7199 rabobankc.westeurope.cloudapp.azure.com
+ssh -N -L 9042:10.1.0.36:9042 -L 7199:10.1.0.36:7199 rabobankjb.westeurope.cloudapp.azure.com
